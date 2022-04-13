7590a2e2a5ab4d0030f469371d24566197d4a9c8
ELTITI84 <thiagogab05@gmail.com>
2022-04-12 23:41:21 -0300

    Initial commit

---
+++ Assets/Scripts/ValidarContrasenia.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContrasenia : MonoBehaviour {
+
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
+    public Text textomsg;
+    public GameObject cartelmsg;
+
+	// Use this for initialization
+	void Start () {
+        contraseniaCorrecta = "12345";
+        cartelmsg.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ValidarContraseña()
+    {
+        contraseniaUsuario = ingresoUsuario.text;
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            cartelmsg.SetActive(true);
+            textomsg.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartelmsg.SetActive(true);
+            textomsg.text = "Contraseña Incorrecta";
+            Debug.Log("Contraseña Incorrecta");
+        }
+    }
+
+}
